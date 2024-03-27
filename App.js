4f1943253ff5eb72f9ce05cf63734672329cfd76
Santhiya <107858783+santhiya-M-csbs@users.noreply.github.com>
2024-03-27 12:49:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Register from './Components/Register';
+
+function App() {
+  return (
+    <>
+      <Register/>
+    </>
+  );
+}
+
+export default App;
